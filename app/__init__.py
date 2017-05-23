424fe5490e6e0b0f20cf851a0ac07d7538169af6
heshangbuxitou <1581277795@qq.com>
2017-05-23 17:46:58 +0800

    add markdown and some display

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,27 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import config
 from flask_login import LoginManager
+from markdown import markdown
+import bleach
+# from flask_pagedown import PageDown
+
+allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                'h1', 'h2', 'h3', 'p']
+
+def email_hide(args):
+    end = args.find('@')
+    return args[:end-3]+'***'+args[end:]
+
+def to_html(text):
+    return bleach.linkify(bleach.clean(markdown(text, output_format='html'), tags=allowed_tags, strip=True))
+
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
+# pagedown = PageDown()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -21,6 +38,12 @@ def create_app(config_name):
     db.init_app(app)
     moment.init_app(app)
     login_manager.init_app(app)
+    # pagedown.init_app(app)
+
+    env = app.jinja_env
+    env.filters['email_hide'] = email_hide
+    env.filters['to_html'] = to_html
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- manage.py
+++ manage.py
@@ -4,14 +4,7 @@ from app.models import User, Post, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-def email_hide(args):
-    end = args.find('@')
-    return args[:end-3]+'***'+args[end:]
-
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-env = app.jinja_env
-env.filters['email_hide'] = email_hide
 
 manager = Manager(app)
 
